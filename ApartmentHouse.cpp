85e3d137d5424a853cc2677ea882ecf7b8eaa839
Andrei Gridin <andreigridin918@gmail.com>
2024-02-08 22:41:18 +0500

    Добавил оформление

--- ApartmentHouse.cpp
+++ ApartmentHouse.cpp
@@ -13,20 +13,20 @@ int main()
     string surname[10];                                          // Объявляем стринговый массив
     cout << " Enter surnames: " << endl;                         // Запрашиваем фамилии жильцов
     for (int i = 0; i < 10; i++) {                               // В цикле заполняем массив фамилиями жильцов
-        cout << " ";                                             
+        cout << " " << (char)1 << " ";
         cin >> surname[i];
     }
 
     while (true) {                                               // Бесконечный цикл для запроса номера квартиры
         int number;                                              // и вывода фамилии жильца таковой
         cout << " Enter apartment number: " << endl;
-        cout << " ";
+        cout << " " << (char)35 << " ";
         cin >> number;
         if (number < 1 || number>10) {                           // Если введён недопустимый номер квартиры
             cout << " Invalid apartment number entered" << endl; // выводим сообщение об этом и выходим из программы
             break;
         }
-        cout << ' ' << surname[number - 1] << endl;
+        cout << " " << (char)2 << " " << surname[number - 1] << endl;
     }
 
 
